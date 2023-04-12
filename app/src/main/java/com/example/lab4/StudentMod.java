fc79eece482c89099fb23c1aaa1bc77faa4145e3
RagadAlhindi <ragadwh17@gmail.com>
2023-04-13 01:28:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab4/StudentMod.java
@@ -0,0 +1,55 @@
+package com.example.lab4;
+
+public class StudentMod {
+
+
+    private int ID ;
+    private String name;
+    private int age;
+
+
+
+
+    public StudentMod(int ID, String name, int age) {
+        this.ID = ID;
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Student Model{" +
+                "ID=" + ID +
+                ", name='" + name + '\'' +
+                ", age=" + age + '}';
+    }
+}
